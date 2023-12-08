88469a173ff1c320ac7cdd2fda74fbf2be3820ec
Migoreo <117596911+migoreo@users.noreply.github.com>
2023-12-08 19:00:23 +0800

    Delete LMSVSC directory

--- LMSVSC/DBconfig.java
+++
@@ -1,5 +0,0 @@
-public class DbConfig {
-    public static final String DB_URL = "jdbc:mysql://localhost:3306/librarymanagementsystem";
-    public static final String DB_USER = "admin";
-    public static final String DB_PASSWORD = "pass";
-}
